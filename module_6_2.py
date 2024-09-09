9e95e11659da666f5733eaad6168721f640124ea
Сергей Нефёдов <24twister555@mail.ru>
2024-09-09 12:54:45 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,46 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['Red', 'Yellow', 'White', 'Black', 'Gray', 'Green']
+
+    def __init__(self, owner, __model, __engine_power, __color):
+        self.owner = str(owner)
+        self.__model = str(__model)
+        self.__engine_power = int(__engine_power)
+        self.__color = str(__color)
+
+    def get_model(self):
+        print(f'Модель: {self.__model}')
+
+    def get_horsepower(self):
+        print(f'Мощность двигателя: {self.__engine_power}')
+
+    def get_color(self):
+        print(f'Цвет: {self.__color.upper()}')
+
+    def print_info(self):
+        self.get_model()
+        self.get_horsepower()
+        self.get_color()
+        print(f'Владелец: {self.owner}\n')
+
+    def set_color(self, new_color):
+        # new_color = input('Новый цвет: ')
+        reg_colours = [colour.lower() for colour in self.__COLOR_VARIANTS]
+        if new_color.lower() in reg_colours:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}\n')
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+
+avto1 = Sedan('Sergey', 'KIA RIO', 104, 'Green')
+
+avto1.print_info()
+
+avto1.set_color('Pink')
+avto1.set_color('BLACK')
+avto1.owner = 'Kristina'
+
+avto1.print_info()
